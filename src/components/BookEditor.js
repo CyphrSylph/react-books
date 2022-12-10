41bd3a1a42548781a9c683d7f3ba0f390da2852e
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-12-09 19:16:30 -0600

    setup edit function to update books in list

--- src/components/BookEditor.js
+++ src/components/BookEditor.js
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
 function BookEditor() {
-    return <div> Book Editor </div>;
+    const [title, setTitle] = useState('');
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("New tilte is: ", title);
+    };
+
+    return (
+        <form className="book-feature" onSubmit={handleSubmit}>
+            <label> Title </label>
+            <input className="input" value={title} onChange={handleChange} />
+            <button className="button is-primary">
+                Save
+            </button>
+        </form>
+    );
 }
 
 export default BookEditor;
--- src/components/BookFeature.js
+++ src/components/BookFeature.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+import BookEditor from './BookEditor';
+
 function BookFeature({book, onDelete}) { 
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
+    }
+    
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    }
+
+    let content = <h3>{book.title}</h3>;
+    if(showEdit) {
+        content = <BookEditor />;
     }
 
     return (
         <div className="book-feature">
-            {book.title} 
+            <div>{content}</div> 
             <div className="actions">
-                <button className="delete" onClick={handleClick}>
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
                     Delete
                 </button>
             </div>
